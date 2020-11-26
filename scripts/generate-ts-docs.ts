b1f7a12fbfeac0a4b3ed5fe8323c912d8180667a
yuanqing <desk@yuanqing.sg>
2020-11-27 07:54:03 +0800

    Rename

--- scripts/generate-ts-docs.ts
+++ scripts/generate-ts-docs.ts
@@ -1,14 +1,14 @@
 import {
   groupFunctionsDataByCategory,
   parseExportedFunctionsAsync,
-  stringifyCategoryToMarkdown
+  renderCategoryAsMarkdown
 } from '../src'
 
 async function main() {
   const functionsData = await parseExportedFunctionsAsync(['./src/*.ts'])
   const categories = groupFunctionsDataByCategory(functionsData)
   for (const category of categories) {
-    console.log(stringifyCategoryToMarkdown(category, { headerLevel: 3 })) // eslint-disable-line no-console
+    console.log(renderCategoryAsMarkdown(category, { headerLevel: 3 })) // eslint-disable-line no-console
   }
 }
 main()
