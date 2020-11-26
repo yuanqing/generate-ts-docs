141779a879401a5150bc9a51be2d2f81ad2e2737
yuanqing <desk@yuanqing.sg>
2020-11-27 07:56:05 +0800

    Remove `eslint-disable-line`

--- example/generate-ts-docs.ts
+++ example/generate-ts-docs.ts
@@ -6,7 +6,7 @@ import {
 async function main() {
   const functionsData = await parseExportedFunctionsAsync(['./example.ts'])
   for (const functionData of functionsData) {
-    console.log(renderFunctionDataAsMarkdown(functionData)) // eslint-disable-line no-console
+    console.log(renderFunctionDataAsMarkdown(functionData))
   }
 }
 main()
