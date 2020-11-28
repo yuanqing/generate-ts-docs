ff4e85b20d261ca56e64b539b3b0dc514fcb2655
yuanqing <desk@yuanqing.sg>
2020-11-28 18:35:04 +0800

    Refactor

---
+++ src/__fixtures__/8-multiple-functions.ts
@@ -0,0 +1,7 @@
+export function add(x: number, y: number): number {
+  return x + y
+}
+
+export function noop(): void {
+  return
+}
--- src/__tests__/parse-exported-functions-async.ts
+++ src/__tests__/parse-exported-functions-async.ts
@@ -22,8 +22,8 @@ test('parses function expressed as a function declaration (`function add`)', asy
   t.plan(2)
   const filePath = path.resolve(fixturesDirectory, '1-function-declaration.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: null,
       name: 'add',
@@ -54,8 +54,8 @@ test('parses function expressed as a variable statement eg. `const foo = functio
   t.plan(2)
   const filePath = path.resolve(fixturesDirectory, '2-variable-statement.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: null,
       name: 'add',
@@ -86,8 +86,8 @@ test('parses optional parameters', async function (t) {
   t.plan(2)
   const filePath = path.resolve(fixturesDirectory, '3-optional-parameters.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: null,
       name: 'foo',
@@ -118,8 +118,8 @@ test('parses the function description', async function (t) {
   t.plan(2)
   const filePath = path.resolve(fixturesDirectory, '4-function-description.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: 'Adds two numbers.',
       name: 'add',
@@ -150,8 +150,8 @@ test('parses the `@param` tag', async function (t) {
   t.plan(2)
   const filePath = path.resolve(fixturesDirectory, '5-param-tag.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: null,
       name: 'add',
@@ -182,8 +182,8 @@ test('parses the `@returns` tag', async function (t) {
   t.plan(2)
   const filePath = path.resolve(fixturesDirectory, '6-return-tag.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: null,
       name: 'add',
@@ -214,8 +214,8 @@ test('parses the `@returns` tag', async function (t) {
   t.plan(2)
   const filePath = path.resolve(fixturesDirectory, '7-category-tag.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: null,
       name: 'add',
@@ -244,12 +244,54 @@ test('parses the `@returns` tag', async function (t) {
   ])
 })
 
+test('parses multiple functions', async function (t) {
+  t.plan(2)
+  const filePath = path.resolve(fixturesDirectory, '8-multiple-functions.ts')
+  t.true((await fs.pathExists(filePath)) === true)
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
+    {
+      description: null,
+      name: 'add',
+      parameters: [
+        {
+          description: null,
+          name: 'x',
+          optional: false,
+          type: 'number'
+        },
+        {
+          description: null,
+          name: 'y',
+          optional: false,
+          type: 'number'
+        }
+      ],
+      returnType: {
+        description: null,
+        type: 'number'
+      },
+      tags: null
+    },
+    {
+      description: null,
+      name: 'noop',
+      parameters: [],
+      returnType: {
+        description: null,
+        type: 'void'
+      },
+      tags: null
+    }
+  ])
+})
+
 test('sorts functions by their `@weight` tag', async function (t) {
   t.plan(2)
-  const filePath = path.resolve(fixturesDirectory, '8-weight-tag.ts')
+  const filePath = path.resolve(fixturesDirectory, '9-weight-tag.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: null,
       name: 'noop',
@@ -292,10 +334,10 @@ test('sorts functions by their `@weight` tag', async function (t) {
 
 test('ignores functions with the `@ignore` tag', async function (t) {
   t.plan(2)
-  const filePath = path.resolve(fixturesDirectory, '9-ignore-tag.ts')
+  const filePath = path.resolve(fixturesDirectory, '10-ignore-tag.ts')
   t.true((await fs.pathExists(filePath)) === true)
-  const results = await parseExportedFunctionsAsync([filePath])
-  t.deepEqual(results, [
+  const functionsData = await parseExportedFunctionsAsync([filePath])
+  t.deepEqual(functionsData, [
     {
       description: null,
       name: 'noop',
@@ -308,37 +350,3 @@ test('ignores functions with the `@ignore` tag', async function (t) {
     }
   ])
 })
-
-// test('complex', async function (t) {
-//   t.plan(2)
-//   const filePath = path.resolve(fixturesDirectory, '5-complex.ts')
-//   t.true(await fs.pathExists(filePath) === true)
-//   const results = await parseExportedFunctionsAsync([filePath])
-//   t.deepEqual(results, [
-//     {
-//       "description": "Description for function `foo`.",
-//       "name": "foo",
-//       "parameters": [
-//         {
-//           "description": "Description for parameter `x`.",
-//           "name": "x",
-//           "optional": false,
-//           "type": "number",
-//         },
-//         {
-//           "description": "Description for parameter `y`.",
-//           "name": "y",
-//           "optional": true,
-//           "type": "string",
-//         },
-//       ],
-//       "returnType": {
-//         "description": "Description for return type.",
-//         "type": "{\n  x: number;\n  y?: string;\n}",
-//       },
-//       "tags": {
-//         "category": "Category for function `foo`.",
-//       },
-//     }
-//   ])
-// })
